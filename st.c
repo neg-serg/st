52680c5e8679a9aaab76a4b48c66159886f9176c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-07-30 21:54:30 +0300

    simplify loop condition

--- st.c
+++ st.c
@@ -864,7 +864,7 @@ char *getsel(void) {
     ptr = str = xmalloc(bufsize);
 
     /* append every set & selected glyph to the selection */
-    for (y = sel.nb.y; y < sel.ne.y + 1; y++) {
+	for(y = sel.nb.y; y <= sel.ne.y; y++) {
         linelen = tlinelen(y);
 
         if (sel.type == SEL_RECTANGULAR) {
