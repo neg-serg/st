5405278148c26f4d7a0c1a2e4925d6345b869037
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-07-31 11:19:57 +0300

    Small bugfix for makeglyphfontspecs call in

--- st.c
+++ st.c
@@ -3375,7 +3375,7 @@ void drawregion(int x1, int y1, int x2, int y2) {
         term.dirty[y] = 0;
 
 		specs = term.specbuf;
-		numspecs = xmakeglyphfontspecs(specs, &term.line[y][0], x2 - x1, x1, y);
+		numspecs = xmakeglyphfontspecs(specs, &term.line[y][x1], x2 - x1, x1, y);
 
 		i = ox = 0;
 		for(x = x1; x < x2 && i < numspecs; x++) {
