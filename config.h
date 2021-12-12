a72d391ebb56b342b4f20a8c427e79ea488f086c
Neg <serg.zorg@gmail.com>
2021-12-13 01:26:40 +0300

    Better font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka:size=32.5:autohint=true";
+static char *font = "Iosevka:size=33:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
