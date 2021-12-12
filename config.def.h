65056b3da0a2b9bf94d91c3c3897de42fdfb642f
Neg <serg.zorg@gmail.com>
2021-12-12 21:38:33 +0300

    Add multifont support

--- config.def.h
+++ config.def.h
@@ -6,6 +6,11 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Iosevka:size=32";
+/* Spare fonts */
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
 static int borderpx = 2;
 
 /*
